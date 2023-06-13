0dba4e833671044267990c97b86fe3406cea3031
Taylor <koo2true@gmail.com>
2023-06-13 14:39:56 -0700

    delete done and get all and get single finsihed?

--- app/api/items_routes.py
+++ app/api/items_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, session, request, redirect
+from app.models.db import db
 from app.models.items import Items
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models.reviews import Reviews
 
 items_routes = Blueprint('items',__name__)
@@ -10,11 +12,27 @@ def get_all_items():
     response = [item.to_dict() for item in all_items]
     return {'item': response}
 
+@items_routes.route('')
+@login_required
+def make_new_item():
+    # form = ItemForm()
+    pass
+
+@items_routes.route('/<:int:id>/delete' ,methods=['DELETE'])
+def edit_item(item_id):
+    item_delete = Items.query.get(item_id)
+
+    if(item_delete is None):
+        return {'message':'Item could not be found'}
+
+    db.session.delete(item_delete)
+    db.session.commit()
+
+    return {'message':'Item deleted'}
+
 
 @items_routes.route('/<int:id>')
 def get_single_item(id):
     single_shoe = Items.query.get(id)
     single_shoe_dict = single_shoe.to_dict()
     return {'item': single_shoe_dict}
-
-
