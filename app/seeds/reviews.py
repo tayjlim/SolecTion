9603c24be6e169b33494afaa2532c0185bbf4e7e
Taylor <koo2true@gmail.com>
2023-06-13 10:03:41 -0700

    weo

---
+++ app/seeds/reviews.py
@@ -0,0 +1,22 @@
+from app.models.db import db, environment, SCHEMA
+from app.models.reviews import Reviews
+from sqlalchemy.sql import text
+
+def seed_reviews():
+    review_1=Reviews(
+        user_id = 1 ,
+        item_id = 1,
+        desc = 'These are forsure a cop!',
+        picture_aws_link = 'https://cdn.discordapp.com/attachments/1118210959077556414/1118223469398724720/Eizn9DnWoAA8YjE.jpg'
+
+    )
+    db.session.add(review_1)
+    db.session.commit()
+
+def undo_reviews():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM reviews"))
+        
+    db.session.commit()
