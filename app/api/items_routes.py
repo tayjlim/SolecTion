a184d270fcfde2a8b392bfc1d45bd133c703a695
Taylor <koo2true@gmail.com>
2023-06-13 14:59:22 -0700

    finished some stuff

--- app/api/items_routes.py
+++ app/api/items_routes.py
@@ -4,6 +4,10 @@ from app.models.items import Items
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models.reviews import Reviews
 
+from app.api.aws_helpers import get_unique_filename,upload_file_to_s3,remove_file_from_s3
+
+from app.forms.post_item_form import ItemForm
+
 items_routes = Blueprint('items',__name__)
 
 @items_routes.route('')
@@ -12,11 +16,32 @@ def get_all_items():
     response = [item.to_dict() for item in all_items]
     return {'item': response}
 
-@items_routes.route('')
+@items_routes.route('/new', methods=['POST'])
 @login_required
 def make_new_item():
-    # form = ItemForm()
-    pass
+    user_id = current_user.id
+    form = ItemForm()
+
+    if form.validate_on_submit():
+        picture=form.data['cover_picture']
+        picture.filename = get_unique_filename(picture.filename)
+        uploaded = upload_file_to_s3(picture.filename)
+        aws_link = uploaded['url']
+        new_item = Items(
+            owner_id= int(user_id),
+            name = form.data['name']
+            desc = form.data['desc']
+            price = form.data['price']
+            picture_aws_link = aws_link
+        )
+
+        db.session.add(new_item)
+        db.session.commiT()
+        return new_item.to_dict()
+    else:
+        return {'error': form.errors}
+
+
 
 @items_routes.route('/<:int:id>/delete' ,methods=['DELETE'])
 def edit_item(item_id):
--- app/forms/post_item_form.py
+++ app/forms/post_item_form.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField,FileField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed,FileRequired
+
 # from app.models import Favorites
 
 class ItemForm(FlaskForm):
-    name = 
+    name = StringField('name',validators=[DataRequired()])
+    desc = StringField('description', validators=[DataRequired()])
+    price = IntegerField('price',validators=[DataRequired()])
+    cover_picture= FileField('cover_picture', validators=[FileRequired(),FileAllowed(['png', 'jpeg', 'jpg'])])
