293a4fbe2f2dc0c0cd9ffed781465bee016f70a2
Taylor <koo2true@gmail.com>
2023-06-14 15:53:20 -0700

    added started delete modal

---
+++ react-app/src/components/DeleteItems/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom"
+import { useHistory } from "react-router-dom"
+// import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
+
+const DeleteItems =  ({item}) => {
+ return(
+    <>
+    swag</>
+ )
+}
+
+
+export default DeleteItems
--- react-app/src/components/ItemsDetail/index.js
+++ react-app/src/components/ItemsDetail/index.js
@@ -3,17 +3,23 @@ import { useParams, NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { getAllItemsThunk } from '../../store/items';
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem.js";
+import DeleteItems from '../DeleteItems'
+
 
 function ItemsDetail(){
+
     const dispatch = useDispatch();
     const {itemId} = useParams();
     const singleItem = useSelector(state=> state.items)[itemId]
-    
+    console.log(singleItem)
+    const user = useSelector((state) => state.session.user);
+    console.log(user)
 
-    // useEffect(() => {
-    //     dispatch(getAllItemsThunk());
 
-    // },[dispatch])
+    useEffect(()=>{
+
+    })
     if (!singleItem) return null
 
     else
@@ -24,6 +30,20 @@ function ItemsDetail(){
             {singleItem.name}
             </h2>
             <img src = ''></img>
+
+            {(user.id === singleItem.owner_id)?
+                <div>
+                <OpenModalMenuItem
+                className="delete-button"
+                itemText="Delete this episode"
+                modalComponent={<DeleteItems item={singleItem} key={`${singleItem.id}-items`} />}
+                />
+
+
+
+                </div> : null}
+
+
 
         </div>
         )
