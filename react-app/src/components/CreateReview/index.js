e1044add6f14c03f33d2d9323da4244295e28447
Taylor <koo2true@gmail.com>
2023-06-16 16:08:35 -0700

    some more styling

--- react-app/src/components/CreateReview/index.js
+++ react-app/src/components/CreateReview/index.js
@@ -1,10 +1,21 @@
+import React, {useEffect,useState} from 'react'
+
+
 function CreateReview () {
+
+    const [cover_picture, setcover_picture] = useState()
+    const [desc, setdesc] = useState()
+    const [price, setprice] = useState()
+
+
     return(
-        <div>
-        <h3>Post an on Feet Picture!</h3>
-        <form>
-        <button>Submit</button>
-        </form>
+        <div className = 'createReviewModalDiv'>
+            <h3>Post a Picture!</h3>
+                <form>
+                <label>How do they feel?</label>
+
+                <button>Submit</button>
+                </form>
         </div>
     )
 
