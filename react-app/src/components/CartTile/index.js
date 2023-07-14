978f20015fcb6b16c7781fc7dac11dd7c70fcd51
Taylor <koo2true@gmail.com>
2023-07-13 17:56:20 -0700

    add items to cart mapped

---
+++ react-app/src/components/CartTile/index.js
@@ -0,0 +1,25 @@
+import { useHistory } from "react-router-dom";
+import './index.css'
+function CartTile ({item}) {
+    const history = useHistory();
+    const handleClick= () =>{
+        history.push(`/items/${item.id}`)
+
+    }
+
+return (
+    <div id = {item.id} className="ItemTileContainer" onClick={handleClick}>
+            <div id = ''className = 'topTile'>
+            <img className = 'shoePicTile' src = {item.picture_aws_link}></img>
+            </div>
+            <div className = 'bottomTile'>
+            <p className = 'textDiv'>{item.name}</p>
+            <h3 className = 'textDiv'>${item.price}</h3>
+            </div>
+
+       </div>
+
+)
+}
+
+export default CartTile;
